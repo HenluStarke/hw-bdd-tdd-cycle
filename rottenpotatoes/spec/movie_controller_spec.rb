1ddf887b764bf4a453e14d541dda6201c43c932d
Henlu Starke <henlu.starke@hetzner.co.za>
2015-11-29 16:45:48 +0200

    part 2 first green cucumber code

---
+++ rottenpotatoes/spec/movie_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require 'movie'
+
+describe 'Movies' do
+  context 'searching movies' do
+    it 'should find movies with the same director as the current movie' do
+    end
+    it 'should show an alert if no similar directors as the current movie were found' do
+    end
+  end
+end
